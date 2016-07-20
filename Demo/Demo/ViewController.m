92d16d80175caae93f56014b7ec11f51532663a3
HuangKai <huangkai@HuangKaideMacBook-Pro.local>
2016-07-20 15:31:10 +0800

    修改README

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -24,14 +24,15 @@
 
 - (IBAction)onMultiDownloader:(id)sender{
     __weak typeof(self) weakSelf = self;
-    [HKMultiFileDownloader hk_downloadWithURL:@"http://ww1.sinaimg.cn/mw690/5993966fjw1f5v1u972lbj20sg0iztby.jpg" completion:^(NSError *error, NSData *data) {
-        if (!error) {
-            UIImage *image = [UIImage imageWithData:data scale:1];
-            weakSelf.imageView.image = image;
-        }else {
-            weakSelf.imageView.image = nil;
-        }
-    }];
+    [HKMultiFileDownloader hk_downloadWithURL:@"http://ww1.sinaimg.cn/mw690/5993966fjw1f5v1u972lbj20sg0iztby.jpg"
+                                   completion:^(NSError *error, NSData *data) {
+                                       if (!error) {
+                                           UIImage *image = [UIImage imageWithData:data scale:1];
+                                           weakSelf.imageView.image = image;
+                                       }else {
+                                           weakSelf.imageView.image = nil;
+                                       }
+                                   }];
 }
 
 
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # HKMultiFileDownloader
 
 用法如下：
-[HKMultiFileDownloader hk_downloadWithURL:@"http://ww1.sinaimg.cn/mw690/5993966fjw1f5v1u972lbj20sg0iztby.jpg" completion:^(NSError *error, NSData *data) {
-        if (!error) {
-            UIImage *image = [UIImage imageWithData:data scale:1];
-            weakSelf.imageView.image = image;
-        }else {
-            weakSelf.imageView.image = nil;
-        }
-    }];
+__weak typeof(self) weakSelf = self;
+[HKMultiFileDownloader hk_downloadWithURL:@"http://ww1.sinaimg.cn/mw690/5993966fjw1f5v1u972lbj20sg0iztby.jpg"
+                               completion:^(NSError *error, NSData *data) {
+                                   if (!error) {
+                                       UIImage *image = [UIImage imageWithData:data scale:1];
+                                       weakSelf.imageView.image = image;
+                                   }else {
+                                       weakSelf.imageView.image = nil;
+                                   }
+                               }];
